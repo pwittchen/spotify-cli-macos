0f5bef83982a6590cc79d71b789b95de842c8ba2
Piotr Wittchen <piotr@wittchen.io>
2025-02-02 20:12:18 +0100

    simplified the script

--- spotifycli.sh
+++ spotifycli.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 
-show_help() {
+if [[ $1 == "activate" ]]; then
+    osascript -e 'tell application "Spotify" to activate'
+    exit
+elif [[ $1 == "status" ]]; then
+    status_value=$(osascript -e 'tell application "System Events" to (name of processes) contains "Spotify"')
+    if [[ $status_value == "true" ]]; then
+        echo "spotify app is running"
+    else
+        echo "spotify app is off"
+    fi
+    exit
+elif [[ $1 == "play" ]]; then
+    osascript -e 'tell application "Spotify" to play'
+    exit
+elif [[ $1 == "pause" ]]; then
+    osascript -e 'tell application "Spotify" to pause'
+    exit
+elif [[ $1 == "playpause" ]]; then
+    osascript -e 'tell application "Spotify" to playpause'
+    exit
+elif [[ $1 == "restart" ]]; then
+    osascript -e 'tell application "Spotify" to set player position to 0'
+    exit
+elif [[ $1 == "next" ]]; then
+    osascript -e 'tell application "Spotify" to next track'
+    exit
+elif [[ $1 == "prev" ]]; then
+    # it has to be invoked twice, otherwise it goes to the beginning of the current track
+    osascript -e 'tell application "Spotify" to previous track'
+    osascript -e 'tell application "Spotify" to previous track'
+    exit
+elif [[ $1 == "track" ]]; then
+    osascript -e 'tell application "Spotify" to name of current track & " - " & artist of current track'
+    exit
+else
     echo "usage: spotifycli <command>"
     echo "  activate    activate spotify"
     echo "  status      check if spotify app is running"
@@ -13,79 +47,5 @@ show_help() {
     echo "  track       show artist and song title"
     echo "  help        show help"
     exit 0
-}
-
-activate() {
-    osascript -e 'tell application "Spotify" to activate'
-}
-
-status() {
-    status_value=$(osascript -e 'tell application "System Events" to (name of processes) contains "Spotify"')
-    if [[ $status_value == "true" ]]; then
-        echo "spotify app is running"
-    else
-        echo "spotify app is off"
-    fi
-}
-
-play() {
-    osascript -e 'tell application "Spotify" to play'
-}
-
-pause() {
-    osascript -e 'tell application "Spotify" to pause'
-}
-
-playpause() {
-    osascript -e 'tell application "Spotify" to playpause'
-}
-
-restart() {
-    osascript -e 'tell application "Spotify" to set player position to 0'
-}
-
-next() {
-    osascript -e 'tell application "Spotify" to next track'
-}
-
-prev() {
-    # it has to be invoked twice, otherwise it goes to the beginning of the current track
-    osascript -e 'tell application "Spotify" to previous track'
-    osascript -e 'tell application "Spotify" to previous track'
-}
-
-track() {
-    osascript -e 'tell application "Spotify" to name of current track & " - " & artist of current track'
-}
-
-if [[ $1 == "activate" ]]; then
-    activate
-    exit
-elif [[ $1 == "status" ]]; then
-    status
-    exit
-elif [[ $1 == "play" ]]; then
-    play
-    exit
-elif [[ $1 == "pause" ]]; then
-    pause
-    exit
-elif [[ $1 == "playpause" ]]; then
-    pause
-    exit
-elif [[ $1 == "restart" ]]; then
-    restart
-    exit
-elif [[ $1 == "next" ]]; then
-    next
-    exit
-elif [[ $1 == "prev" ]]; then
-    prev
-    exit
-elif [[ $1 == "track" ]]; then
-    track
-    exit
-else
-    show_help
 fi
 
